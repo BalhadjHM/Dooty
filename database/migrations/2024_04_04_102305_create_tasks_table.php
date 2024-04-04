d677da2a965493ed2a84a2cdd7fbfe96e3c37d4e
Balhadj <ebalhadj@gmail.com>
2024-04-04 12:32:49 +0200

    Add the database structure
    - edit the user table
    - create the spaces table
    - create the tasks table

---
+++ database/migrations/2024_04_04_102305_create_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('space_id')->constrained()->onDelete('cascade');
+            $table->string('task_name');
+            $table->text('task_description')->nullable();
+            $table->dateTime('task_due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
