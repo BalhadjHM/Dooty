07462a8d6919cb8b73c1d1608700b08de4267d62
Balhadj <ebalhadj@gmail.com>
2024-04-09 17:24:09 +0200

    Edit the Space components and add a call to action buttons pin, edit, delete

--- database/migrations/2024_04_04_102037_create_spaces_table.php
+++ database/migrations/2024_04_04_102037_create_spaces_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('space_name');
+            $table->text('space_description')->nullable();
             $table->json('tags')->nullable();
             $table->timestamps();
         });
